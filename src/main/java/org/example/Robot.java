7c4a348fc702824ed90c980b34c5098774d0cba0
unknown <mbisovskyi@gmail.com>
2022-08-19 13:02:02 -0400

    Class Robot created + Constructor added

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Robot {
+
+    // Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+    // Constructor
+    public Robot() {
+        this.name = "RoboNick";
+        this.weaponName = "Laser Saw";
+        this.weaponPower = 50;
+    }
+}
